52406ea8a40a78db804fae4e6697d42b7904d992
Thiago Marinho <tgmarinho@gmail.com>
2020-04-13 17:02:55 -0400

    Nível 2 - Construindo Aplicação GoBarber API - Criando repositórios (Pattern Repository)

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,28 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentRepository;
