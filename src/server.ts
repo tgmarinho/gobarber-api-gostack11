7ecd33ba86250b2b84d61b7afe487cdfd9604d7a
Thiago Marinho <tgmarinho@gmail.com>
2020-04-13 11:22:26 -0400

    Nível 2 - Node - ESLint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Hi World'})
-})
+app.get('/', (request, response) => response.json({ message: 'Hi World' }));
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
