4de4a7fada55160cf987451584989370671d451a
Thiago Marinho <tgmarinho@gmail.com>
2020-04-13 16:25:57 -0400

    Nível 2 - Construindo Aplicação GoBarber API - Validando Data

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,13 +1,33 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+interface Appointment {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  const appointment = { id: uuid(), provider, date };
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment: Appointment = { id: uuid(), provider, date: parsedDate };
 
   appointments.push(appointment);
 
--- src/server.ts
+++ src/server.ts
@@ -7,5 +7,6 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {
+  // eslint-disable-next-line no-console
   console.log('🚀 Server started on port 3333!');
 });
