48d6edc70bf1e8a01e81ebe18b6bf855028ed641
Thiago Marinho <tgmarinho@gmail.com>
2020-04-14 16:01:30 -0400

    Iniciando back-end - Cadastro de Usuários - criptografia de senha

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,6 +11,8 @@ usersRouter.post('/', async (request, response) => {
 
     const user = await createUser.execute({ name, email, password });
 
+    delete user.password;
+
     return response.json(user);
   } catch (error) {
     return response.status(400).json({ error: error.message });
--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -17,7 +18,13 @@ class CreateUserService {
       throw new Error('Email address already used.');
     }
 
-    const user = userRepository.create({ name, email, password });
+    const hashedPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     await userRepository.save(user);
 
