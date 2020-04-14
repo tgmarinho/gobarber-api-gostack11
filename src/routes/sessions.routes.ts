00deedcc8f3a01199bc3867ee3b4a611c1d71160
Thiago Marinho <tgmarinho@gmail.com>
2020-04-14 18:45:51 -0400

    Iniciando back-end - Autenticação - gerando token JWT

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -10,9 +10,12 @@ sessionsRouter.post('/', async (request, response) => {
 
     const authenticateUserService = new AuthenticateUserService();
 
-    const { user } = await authenticateUserService.execute({ email, password });
+    const { user, token } = await authenticateUserService.execute({
+      email,
+      password,
+    });
 
-    return response.json({ user });
+    return response.json({ user, token });
   } catch (error) {
     return response.status(400).json({ error: error.message });
   }
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 
 interface Request {
@@ -7,8 +8,13 @@ interface Request {
   password: string;
 }
 
+interface Response {
+  user: User;
+  token: string;
+}
+
 class AuthenticateUserService {
-  public async execute({ email, password }: Request): Promise<{ user: User }> {
+  public async execute({ email, password }: Request): Promise<Response> {
     const usersRepository = getRepository(User);
 
     const user = await usersRepository.findOne({ where: { email } });
@@ -23,9 +29,14 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, 'secret_key@156365ff665ce97ce97774e058d33667', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     delete user.password;
 
-    return { user };
+    return { user, token };
   }
 }
 
