862280ec896ca151a11c948d6f6dc9468a495f80
Thiago Marinho <tgmarinho@gmail.com>
2020-05-06 10:11:25 -0400

    GoBarber Node - Testes e TDD - Testando criação de usuário

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,35 +1,35 @@
-import { getRepository, Repository } from 'typeorm';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import User from '../entities/User';
+import { uuid } from 'uuidv4';
+import User from '../../infra/typeorm/entities/User';
 
 class UsersRepository implements IUsersRepository {
-  private ormRepository: Repository<User>;
-
-  constructor() {
-    this.ormRepository = getRepository(User);
-  }
+  private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
-    const user = await this.ormRepository.findOne(id);
-    return user;
+    return this.users.find(user => user.id === id);
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    const user = await this.ormRepository.findOne({ where: { email } });
-    return user;
+    return this.users.find(user => user.email === email);
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
-    const user = this.ormRepository.create(userData);
+    const user = new User();
 
-    await this.ormRepository.save(user);
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
 
     return user;
   }
 
   public async save(user: User): Promise<User> {
-    return this.ormRepository.save(user);
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
   }
 }
 
---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,35 @@
+import AppError from '@shared/errors/AppError';
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'Thiago Marinho',
+      email: 'tgmarinho@gmail.com',
+      password: '123456',
+    });
+
+    expect(user).toHaveProperty('id');
+    expect(user).toHaveProperty('password');
+    expect(user.email).toBe('tgmarinho@gmail.com');
+  });
+
+  it('should not be able to create new user with the same email from another', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const inputUser = {
+      name: 'Thiago Marinho',
+      email: 'tgmarinho@gmail.com',
+      password: '123456',
+    };
+
+    await createUser.execute(inputUser);
+
+    expect(createUser.execute(inputUser)).rejects.toBeInstanceOf(AppError);
+  });
+});
