95898b5501dd5221b97b9a673a502f37c7eacbbb
Thiago Marinho <tgmarinho@gmail.com>
2020-05-14 11:49:47 -0400

    GoBarber Node - Personalizando para producao - Upload de arquivos para amazon s3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,20 +1,41 @@
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tempFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+  config: { disk: {}; aws: { bucket: string } };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tempFolder,
   uploadsFolder: path.resolve(tempFolder, 'uploads'),
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
 
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
-
-      return callback(null, fileName);
+        return callback(null, fileName);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-2',
     },
-  }),
-};
+  },
+} as IUploadConfig;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -8,7 +8,7 @@ import UsersController from '../controllers/UsersController';
 import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
-const upload = multer(uploadConfig);
+const upload = multer(uploadConfig.multer);
 
 const usersController = new UsersController();
 const userAvatarController = new UserAvatarController();
