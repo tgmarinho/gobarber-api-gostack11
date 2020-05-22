7710b5c296335a1f03a86fe154ee999505c0d10b
Thiago Marinho <tgmarinho@gmail.com>
2020-05-22 14:10:17 -0400

    GoBarber Web - Ajustes na API - Clientes dos agendamentos - trazendo os dados do usuário *avatar

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -49,6 +49,7 @@ class AppointmentRepository implements IAppointmentsRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
